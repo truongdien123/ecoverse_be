6c925f5b22098743a49c736839b0893f14991e09
Trương Châu Diễn <dienrengoku148@gmail.com>
2026-04-03 23:04:22 +0700

    get list partners by status

--- src/main/java/com/fpt/ecoverse_backend/repositories/PartnerRepository.java
+++ src/main/java/com/fpt/ecoverse_backend/repositories/PartnerRepository.java
@@ -1,10 +1,16 @@
 package com.fpt.ecoverse_backend.repositories;
 
 import com.fpt.ecoverse_backend.entities.Partner;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PartnerRepository extends JpaRepository<Partner, String> {
 
+    @Query("select p from Partner p where (:status is null or p.status = :status)")
+    Page<Partner> findParntersByStatus(@Param("status") String status, Pageable pageable);
 }
