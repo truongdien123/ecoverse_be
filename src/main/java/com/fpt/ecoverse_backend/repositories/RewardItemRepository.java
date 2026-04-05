dc089e8392097313295ade00349e1ad553955d2d
Trương Châu Diễn <dienrengoku148@gmail.com>
2026-04-05 23:37:18 +0700

    add annotation param in reward item repository

--- src/main/java/com/fpt/ecoverse_backend/repositories/RewardItemRepository.java
+++ src/main/java/com/fpt/ecoverse_backend/repositories/RewardItemRepository.java
@@ -5,11 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RewardItemRepository extends JpaRepository<RewardItem, String> {
 
     @Query("SELECT r FROM RewardItem r WHERE r.partner.id = :partnerId")
-    Page<RewardItem> findRewardItems(String partnerId, Pageable pageable);
+    Page<RewardItem> findRewardItems(@Param("partnerId") String partnerId, Pageable pageable);
 }
